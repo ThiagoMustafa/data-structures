412544addb07580e032320c9cdb3810fa3de9aa0
Thiago Mustafá <thiago.a.mustafa@gmail.com>
2021-05-02 01:06:49 -0300

    Added stack sample.

---
+++ StackSample/OperationsHistory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackSample
+{
+    public class OperationsHistory
+    {
+        private Stack<string> _history;
+        public string UserName { get; private set; }
+
+        public OperationsHistory(string userName)
+        {
+            UserName = userName;
+            _history = new Stack<string>();
+        }
+
+        public void Add(string name)
+        {
+            _history.Push(name);
+        }
+
+        public void Undo()
+        {
+            Console.WriteLine("Last operation undone.");
+            _history.Pop();
+        }
+
+        public string ReturnLastOperation()
+        {
+            var lastOperation = _history.Peek();
+
+            return lastOperation;
+        }
+
+        public void ShowLastOperation() {
+            Console.WriteLine(ReturnLastOperation());
+        }
+
+    }
+}
---
+++ StackSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StackSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var operationsHistory = new OperationsHistory("Thiago Mustafá");
+
+            operationsHistory.Add("Started an article.");
+            operationsHistory.Add("Wrote: Lorem ipsum dolor sit amet, consectetur adipiscing elit");
+            operationsHistory.Add("In fermentum dictum ipsum et tempor. Proin id enim quis nisi pretium rutrum.");
+
+            operationsHistory.Undo();
+
+            operationsHistory.ShowLastOperation();
+
+            Console.ReadLine();
+
+        }
+    }
+}
