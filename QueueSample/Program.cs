e17db45098c7e922d6c9e7b72ac7e2f8054b69f8
Thiago Mustafá <thiago.a.mustafa@gmail.com>
2021-05-02 00:38:20 -0300

    Added queue sample

---
+++ QueueSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace QueueSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceQueueManager = new ServiceQueueManager();
+
+            serviceQueueManager.CreateServiceNumber();
+            serviceQueueManager.CreateServiceNumber();
+            serviceQueueManager.CreateServiceNumber();
+
+            serviceQueueManager.ShowServiceQueue();
+
+            serviceQueueManager.CallNext();
+            serviceQueueManager.CallNext();
+
+            serviceQueueManager.ShowServiceQueue();
+
+            serviceQueueManager.CreateServiceNumber();
+
+            serviceQueueManager.CallNext();
+
+            serviceQueueManager.ShowServiceQueue();
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ QueueSample/ServiceQueueManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueSample
+{
+    public class ServiceQueueManager
+    {
+        private Queue<int> _serviceQueue;
+        private int _nextServiceNumber = 1;
+
+        public ServiceQueueManager()
+        {
+            _serviceQueue = new Queue<int>();
+        }
+
+        public int CreateServiceNumber()
+        {
+            _serviceQueue.Enqueue(_nextServiceNumber);
+
+            Console.WriteLine("Service number created.");
+
+            return _nextServiceNumber++;
+        }
+
+        public void CallNext()
+        {
+            var nextServiceNumber = _serviceQueue.Dequeue();
+
+            Console.WriteLine($"Next service number: {nextServiceNumber}");
+        }
+
+        public void ShowServiceQueue()
+        {
+            Console.Write("Service numbers of queue:");
+
+            foreach (var serviceNumber in _serviceQueue)
+            {
+                Console.Write(serviceNumber + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public void RestartQueue()
+        {
+            _serviceQueue.Clear();
+        }
+    }
+}
